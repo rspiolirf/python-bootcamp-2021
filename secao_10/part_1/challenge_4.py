0514ef5bb991366af6faed719528a02a5a647f24
Rafael Pioli <rspioli.rf@gmail.com>
2021-02-25 16:43:27 -0300

    Resolucao Seçao 10 - Challenge 4

--- secao_10/part_1/challenge_4.py
+++ secao_10/part_1/challenge_4.py
@@ -7,3 +7,10 @@ Make it to print back the same string with the words in backward order.
 For example, say I type the string: My name is Andrei
 Then the script should print out: Andrei is name My
 '''
+
+long_string = input('Entre com uma frase: ')
+long_string_list = long_string.split(' ')
+long_string_list.reverse()
+
+for word in long_string_list:
+    print(word, end=' ')
